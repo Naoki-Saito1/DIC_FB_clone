a7fdc5ec39df8eeefc5aecd2d95a25fe811f994c
saito naoki <reisa.naoki@icloud.com>
2021-09-16 19:37:12 +0900

    contorollers作成

---
+++ db/migrate/20210916013522_create_feeds.rb
@@ -0,0 +1,11 @@
+class CreateFeeds < ActiveRecord::Migration[6.1]
+  def change
+    create_table :feeds do |t|
+      t.text :image
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+
+
+ActiveRecord::Schema.define(version: 2021_09_16_030952) do
+
+ 
+  enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_feeds_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "feeds", "users"
+end
